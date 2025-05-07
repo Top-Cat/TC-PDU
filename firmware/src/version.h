322af0d9b4f5d4c577518202b8ca53a87075f017
Thomas Cheyney <top_cat@thomasc.co.uk>
2025-05-07 17:10:09 +0100

    Bump version

--- firmware/src/version.h
+++ firmware/src/version.h
@@ -7,7 +7,7 @@
   #warning "Build number not set"
   #undef BUILD_NUMBER
   #define SNAPSHOT -SNAPSHOT
-  #define ASSET_VERSION 24
+  #define ASSET_VERSION 27
   #define BUILD_NUMBER ASSET_VERSION
 #else
   #define ASSET_VERSION BUILD_NUMBER
